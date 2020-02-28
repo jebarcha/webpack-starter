353fa9c3767ab0b06d507fd55e635bbf9ac98526
Jebarcha <jebarcha@hotmail.com>
2020-02-28 10:16:11 -0600

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import { saludar } from './js/componentes';
 import './styles.css';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Jose'
+export const todoList = new TodoList();
 
-saludar(nombre);
+// todoList.todos.forEach(todo => crearTodoHtml(todo));
+todoList.todos.forEach(crearTodoHtml);
+
+const newTodo = new Todo('test');
+todoList.todos[0].imprimirClase();
+
+//console.log(todoList.todos);
+
+// const tarea = new Todo('Aprender JavaScript!!');
+// todoList.nuevoTodo(tarea);
+
+//console.log(todoList);
+
+//crearTodoHtml(tarea);
+
+//localStorage.setItem('my-key', JSON.stringify(todoList));
+// setTimeout(() => {
+//     localStorage.removeItem('my-key')
+// }, 2000)
